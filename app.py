20cef9fae6d00009be0b3aa70d18183c562446f6
ajvin <ajvinuez@outlook.com>
2024-01-02 02:57:15 -0500

    update app.py

--- app.py
+++ app.py
@@ -6,14 +6,19 @@ st.header('Vehicle Data Viewer')  # Se agrega Encabezado de la App
 
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
 
-hist_button = st.button('Construir histograma')  # crear un botón
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+    
+'''hist_button = st.button('Construir histograma')  # crear un botón
 
 if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
     st.write(
         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-
+'''
     # crear un histograma
     fig = px.histogram(car_data, x="odometer")
 
