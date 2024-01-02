0b7bae044cb9c68f3859c8efe9eeeedc080c7629
ajvin <ajvinuez@outlook.com>
2024-01-02 03:04:51 -0500

    update app.py

--- app.py
+++ app.py
@@ -9,9 +9,14 @@ car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 # crear una casilla de verificación
 build_histogram = st.checkbox('Construir un histograma')
 
-if build_histogram: # si la casilla de verificación está seleccionada
+if build_histogram:  # si la casilla de verificación está seleccionada
     st.write('Construir un histograma para la columna odómetro')
-    
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
 '''hist_button = st.button('Construir histograma')  # crear un botón
 
 if hist_button:  # al hacer clic en el botón
@@ -19,8 +24,3 @@ if hist_button:  # al hacer clic en el botón
     st.write(
         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 '''
-    # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
-
-    # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
