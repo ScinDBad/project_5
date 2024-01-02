ed492bd70bfa65ee5e1cee33e4fe260bcba08bd5
ajvin <ajvinuez@outlook.com>
2024-01-02 03:37:41 -0500

    update: scatterplot

--- app.py
+++ app.py
@@ -12,9 +12,18 @@ build_scatterplot = st.checkbox('Construir un diagrama de dispersión')
 
 if build_histogram:  # si la casilla de verificación está seleccionada
     st.write('Construir un histograma para la columna odómetro')
-    
+
     # crear un histograma
     fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+if build_scatterplot:
+    st.write('Construir un diagrama de dispersión de precio vs kilometraje')
+
+    # crear scatterplot
+    fig = px.scatter(car_data, x="odometer", y='price')
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
